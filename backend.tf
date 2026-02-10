21ed04991dfa8f9eef20cdc62902a075900c1fa9
Chedva-Gartner <C0556706776@GMAIL.COM>
2026-02-10 22:36:11 +0200

    Change S3 backend region to 'eu-west-1'
    Updated the region in the S3 backend configuration from 'ap-south-1' to 'eu-west-1'.

--- backend.tf
+++ backend.tf
@@ -2,13 +2,11 @@ terraform {
   backend "s3" {
     bucket         = "tf-state-eks"
     key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "eu-west-1"
     dynamodb_table = "tf-locks"
     encrypt        = true
   }
-}
-  provider "aws" {
-    region = "ap-south-1"
-}
+
+
 
 
