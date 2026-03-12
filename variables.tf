a77d69d9fc7ae210c6462ec881a46c97953af184
Chedva-Gartner <C0556706776@GMAIL.COM>
2026-03-12 11:28:11 +0200

    Enhance variable definitions with types and descriptions
    Added descriptions and types for various variables in the Terraform configuration.

--- variables.tf
+++ variables.tf
@@ -1,14 +1,59 @@
-variable "region" {}
-variable "cluster_name" {}
-variable "vpc_cidr" {}
+variable "region" {
+  type        = string
+  description = "The AWS region where resources will be created."
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "The name of the EKS cluster."
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "The CIDR block for the VPC."
+}
+
 variable "azs" {
-  type = list(string)
+  type        = list(string)
+  description = "A list of availability zones in the region."
 }
 
 variable "private_subnets" {
-  type = list(string)
+  type        = list(string)
+  description = "A list of private subnets inside the VPC."
 }
 
 variable "public_subnets" {
-  type = list(string)
+  type        = list(string)
+  description = "A list of public subnets inside the VPC."
+}
+
+variable "node_group_name" {
+  type        = string
+  description = "The name of the EKS node group."
+}
+
+variable "node_role_arn" {
+  type        = string
+  description = "The ARN of the IAM role for the EKS nodes."
+}
+
+variable "instance_types" {
+  type        = list(string)
+  description = "The instance types for the EKS nodes."
+}
+
+variable "desired_capacity" {
+  type        = number
+  description = "The desired number of nodes in the node group."
+}
+
+variable "min_capacity" {
+  type        = number
+  description = "The minimum number of nodes in the node group."
+}
+
+variable "max_capacity" {
+  type        = number
+  description = "The maximum number of nodes in the node group."
 }
